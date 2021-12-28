Zeral-Zhang/appsmith

commit e97e434dae999d7dd531f677ebf8f270e1f6cadc

feat: adding ads component for UQI (#8777)

src/components/ads/Dropdown.tsx
@@ -14,6 +14,8 @@ import styled from "constants/DefaultTheme";
 import SearchComponent from "components/designSystems/appsmith/SearchComponent";
 import { Colors } from "constants/Colors";
 import Spinner from "./Spinner";
+import { ReactComponent as Check } from "assets/icons/control/checkmark.svg";
+import { ReactComponent as Close } from "assets/icons/control/remove.svg";
 import { replayHighlightClass } from "globalStyles/portals";
 import Tooltip from "components/ads/Tooltip";
 import { isEllipsisActive } from "utils/helpers";
@@ -44,7 +46,7 @@ export interface DropdownSearchProps {
 
 export interface RenderDropdownOptionType {
   index?: number;
-  option: DropdownOption;
+  option: DropdownOption | DropdownOption[];
   optionClickHandler?: (dropdownOption: DropdownOption) => void;
   isSelectedNode?: boolean;
   extraProps?: any;
@@ -63,8 +65,9 @@ type RenderOption = ({
 export type DropdownProps = CommonComponentProps &
   DropdownSearchProps & {
     options: DropdownOption[];
-    selected: DropdownOption;
+    selected: DropdownOption | DropdownOption[];
     onSelect?: DropdownOnSelect;
+    isMultiSelect?: boolean;
     width?: string;
     height?: string;
     showLabelOnly?: boolean;
@@ -91,16 +94,19 @@ export type DropdownProps = CommonComponentProps &
     fillOptions?: boolean;
     dontUsePortal?: boolean;
     hideSubText?: boolean;
+    removeSelectedOption?: DropdownOnSelect;
     boundary?: PopperBoundary;
     defaultIcon?: IconName;
     truncateOption?: boolean; // enabled wrapping and adding tooltip on option item of dropdown menu
   };
 export interface DefaultDropDownValueNodeProps {
-  selected: DropdownOption;
+  selected: DropdownOption | DropdownOption[];
   showLabelOnly?: boolean;
+  isMultiSelect?: boolean;
   isOpen?: boolean;
   hasError?: boolean;
   renderNode?: RenderOption;
+  selectedOptionClickHandler?: (option: DropdownOption) => void;
   placeholder?: string;
   showDropIcon?: boolean;
   optionWidth: string;
@@ -108,13 +114,13 @@ export interface DefaultDropDownValueNodeProps {
 }
 
 export interface RenderDropdownOptionType {
-  option: DropdownOption;
+  option: DropdownOption | DropdownOption[];
   optionClickHandler?: (dropdownOption: DropdownOption) => void;
 }
 
 export const DropdownContainer = styled.div<{ width: string; height?: string }>`
   width: ${(props) => props.width};
-  height: ${(props) => props.height || `38px`};
+  min-height: ${(props) => props.height};
   position: relative;
   span.bp3-popover-target {
     display: inline-block;
@@ -146,6 +152,41 @@ const DropdownTriggerWrapper = styled.div<{
   }
 `;
 
+const StyledCheckmark = styled(Check)`
+  width: 14px;
+  height: 14px;
+  position: absolute;
+  top: -1px;
+  left: -1px;
+`;
+
+const StyledClose = styled(Close)`
+  width: 24px;
+  height: 24px;
+  padding: 3px;
+  padding-right: 10px;
+  &:hover {
+    background-color: #ebebeb;
+  }
+`;
+const SquareBox = styled.div<{
+  backgroundColor?: string;
+  borderColor?: string;
+}>`
+  width: 14px;
+  height: 14px;
+  box-sizing: border-box;
+  position: relative;
+  margin-right: 10px;
+  background-color: ${(props) =>
+    props.backgroundColor ? props.backgroundColor : "transparent"};
+  border: ${(props) =>
+    props.borderColor
+      ? `1.8px solid ${props.borderColor}`
+      : "1.8px solid #A9A7A7"};
+  border-width: 1.8px;
+`;
+
 const Selected = styled.div<{
   isOpen: boolean;
   disabled?: boolean;
@@ -154,6 +195,7 @@ const Selected = styled.div<{
   hasError?: boolean;
   selected?: boolean;
   isLoading?: boolean;
+  isMultiSelect?: boolean;
 }>`
   padding: ${(props) => props.theme.spaces[2]}px
     ${(props) => props.theme.spaces[3]}px;
@@ -170,7 +212,7 @@ const Selected = styled.div<{
   align-items: center;
   justify-content: space-between;
   width: 100%;
-  height: ${(props) => props.height};
+  min-height: ${(props) => props.height};
   cursor: ${(props) =>
     props.disabled || props.isLoading ? "not-allowed" : "pointer"};
   ${(props) =>
@@ -209,11 +251,11 @@ const Selected = styled.div<{
   }
   &:hover {
     background: ${(props) =>
-      props.hasError
-        ? Colors.FAIR_PINK
-        : props.theme.colors.dropdown.hovered.bg};
-    border: 1px solid var(--appsmith-input-focus-border-color);
-  }
+      !props.isMultiSelect
+        ? props.hasError
+          ? Colors.FAIR_PINK
+          : props.theme.colors.dropdown.hovered.bg
+        : Colors.WHITE}
 `;
 
 const DropdownSelect = styled.div``;
@@ -284,7 +326,6 @@ const OptionWrapper = styled.div<{
   align-items: center;
   min-height: 36px;
   background-color: ${(props) => (props.selected ? Colors.GREEN_3 : null)};
-
   &&& svg {
     rect {
       fill: ${(props) => props.theme.colors.dropdownIconBg};
@@ -384,7 +425,7 @@ const SelectedDropDownHolder = styled.div`
   display: flex;
   align-items: center;
   min-width: 0;
-  width: 100%;
+  max-width: 100%;
   overflow: hidden;
 
   & ${Text} {
@@ -403,7 +444,6 @@ const SelectedIcon = styled(Icon)`
     svg {
       height: 18px;
       width: 18px;
-
       rect {
         fill: ${(props) => props.theme.colors.dropdownIconBg};
         rx: 0;
@@ -479,26 +519,64 @@ function TooltipWrappedText(
 function DefaultDropDownValueNode({
   hasError,
   hideSubText,
+  isMultiSelect,
   optionWidth,
   placeholder,
   renderNode,
   selected,
+  selectedOptionClickHandler,
   showDropIcon,
   showLabelOnly,
 }: DefaultDropDownValueNodeProps) {
-  const LabelText = selected
-    ? showLabelOnly
-      ? selected.label
-      : selected.value
-    : placeholder
-    ? placeholder
-    : "Please select a option.";
+  const LabelText =
+    !Array.isArray(selected) && selected
+      ? showLabelOnly
+        ? selected.label
+        : selected.value
+      : placeholder
+      ? placeholder
+      : "Please select a option.";
+
   function Label() {
-    return hasError ? (
-      <ErrorLabel>{LabelText}</ErrorLabel>
-    ) : (
-      <Text type={TextType.P1}>{LabelText}</Text>
-    );
+    if (isMultiSelect && Array.isArray(selected) && selected.length) {
+      return (
+        <div style={{ display: "flex", width: "100%", flexWrap: "wrap" }}>
+          {selected?.map((s: DropdownOption) => {
+            return (
+              <div
+                key={s.value}
+                style={{
+                  border: "1.2px solid #E0DEDE",
+                  display: "flex",
+                  alignItems: "center",
+                  lineHeight: "19px",
+                  margin: "2px 2px",
+                }}
+              >
+                <span style={{ padding: "3px" }}>
+                  <Text type={TextType.P1}>{s.label}</Text>
+                </span>
+                <StyledClose
+                  onClick={(event: any) => {
+                    event.stopPropagation();
+                    if (selectedOptionClickHandler) {
+                      selectedOptionClickHandler(s as DropdownOption);
+                    }
+                  }}
+                />
+              </div>
+            );
+          })}
+        </div>
+      );
+    } else
+      return hasError ? (
+        <ErrorLabel>{LabelText}</ErrorLabel>
+      ) : (
+        <span style={{ width: "100%" }}>
+          <Text type={TextType.P1}>{LabelText}</Text>
+        </span>
+      );
   }
 
   return (
@@ -510,29 +588,33 @@ function DefaultDropDownValueNode({
           hasError,
           optionWidth,
         })
+      ) : isMultiSelect && Array.isArray(selected) && selected.length ? (
+        <Label />
       ) : (
-        <>
-          {selected?.icon ? (
-            <SelectedIcon
-              fillColor={hasError ? Colors.POMEGRANATE2 : selected?.iconColor}
-              hoverFillColor={
-                hasError ? Colors.POMEGRANATE2 : selected?.iconColor
-              }
-              name={selected.icon}
-              size={selected.iconSize || IconSize.XL}
-            />
-          ) : null}
-          <Label />
-          {selected?.subText && !hideSubText ? (
-            <StyledSubText
-              className="sub-text"
-              showDropIcon={showDropIcon}
-              type={TextType.P1}
-            >
-              {selected.subText}
-            </StyledSubText>
-          ) : null}
-        </>
+        !Array.isArray(selected) && (
+          <>
+            {selected?.icon ? (
+              <SelectedIcon
+                fillColor={hasError ? Colors.POMEGRANATE2 : selected?.iconColor}
+                hoverFillColor={
+                  hasError ? Colors.POMEGRANATE2 : selected?.iconColor
+                }
+                name={selected.icon}
+                size={selected.iconSize || IconSize.XL}
+              />
+            ) : null}
+            <Label />
+            {selected?.subText && !hideSubText ? (
+              <StyledSubText
+                className="sub-text"
+                showDropIcon={showDropIcon}
+                type={TextType.P1}
+              >
+                {selected.subText}
+              </StyledSubText>
+            ) : null}
+          </>
+        )
       )}
     </SelectedDropDownHolder>
   );
@@ -542,8 +624,9 @@ interface DropdownOptionsProps extends DropdownProps, DropdownSearchProps {
   optionClickHandler: (option: DropdownOption) => void;
   renderOption?: RenderOption;
   headerLabel?: string;
-  selected: DropdownOption;
+  selected: DropdownOption | DropdownOption[];
   optionWidth: string;
+  isMultiSelect?: boolean;
 }
 
 export function RenderDropdownOptions(props: DropdownOptionsProps) {
@@ -594,12 +677,25 @@ export function RenderDropdownOptions(props: DropdownOptionsProps) {
               optionWidth,
             });
           }
+          let isSelected = false;
+          if (
+            props.isMultiSelect &&
+            Array.isArray(props.selected) &&
+            props.selected.length
+          ) {
+            isSelected = !!props.selected.find(
+              (selectedOption) => selectedOption.value === option.value,
+            );
+          } else {
+            isSelected =
+              (props.selected as DropdownOption).value === option.value;
+          }
           return !option.isSectionHeader ? (
             <OptionWrapper
               className="t--dropdown-option"
               key={index}
               onClick={() => props.optionClickHandler(option)}
-              selected={props.selected.value === option.value}
+              selected={isSelected}
             >
               {option.leftElement && (
                 <LeftIconWrapper>{option.leftElement}</LeftIconWrapper>
@@ -612,7 +708,15 @@ export function RenderDropdownOptions(props: DropdownOptionsProps) {
                   size={option.iconSize || IconSize.XL}
                 />
               ) : null}
-
+              {props.isMultiSelect ? (
+                isSelected ? (
+                  <SquareBox backgroundColor="#f86a2b" borderColor="#f86a2b">
+                    <StyledCheckmark />
+                  </SquareBox>
+                ) : (
+                  <SquareBox borderColor="#a9a7a7" />
+                )
+              ) : null}
               {props.showLabelOnly ? (
                 props.truncateOption ? (
                   <TooltipWrappedText
@@ -635,7 +739,6 @@ export function RenderDropdownOptions(props: DropdownOptionsProps) {
               ) : (
                 <Text type={TextType.P1}>{option.value}</Text>
               )}
-
               {option.subText ? (
                 <StyledSubText type={TextType.P3}>
                   {option.subText}
@@ -664,10 +767,13 @@ export default function Dropdown(props: DropdownProps) {
     errorMsg = "",
     placeholder,
     helperText,
+    removeSelectedOption,
     hasError,
   } = { ...props };
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const [selected, setSelected] = useState<DropdownOption>(props.selected);
+  const [selected, setSelected] = useState<DropdownOption | DropdownOption[]>(
+    props.selected,
+  );
 
   const closeIfOpen = () => {
     if (isOpen) {
@@ -682,12 +788,40 @@ export default function Dropdown(props: DropdownProps) {
 
   const optionClickHandler = useCallback(
     (option: DropdownOption) => {
-      setSelected(option);
+      if (props.isMultiSelect) {
+        // Multi select -> typeof selected is array of objects
+        if (!selected) {
+          setSelected([option]);
+        } else {
+          const newOptions: DropdownOption[] = [
+            ...(selected as DropdownOption[]),
+            option,
+          ];
+          setSelected(newOptions);
+        }
+      } else {
+        // Single select -> typeof selected is object
+        setSelected(option);
+      }
       setIsOpen(false);
       onSelect && onSelect(option.value, option);
       option.onSelect && option.onSelect(option.value, option);
     },
     [onSelect],
+  );
+
+  //Removes selected option
+  const selectedOptionClickHandler = useCallback(
+    (optionToBeRemoved: DropdownOption) => {
+      setIsOpen(false);
+      const selectedOptions = (selected as DropdownOption[]).filter(
+        (option: DropdownOption) => option.value !== optionToBeRemoved.value,
+      );
+      setSelected(selectedOptions);
+      removeSelectedOption &&
+        removeSelectedOption(optionToBeRemoved.value, optionToBeRemoved);
+    },
+    [removeSelectedOption],
   );
 
   const errorFlag = hasError || errorMsg.length > 0;
@@ -734,7 +868,8 @@ export default function Dropdown(props: DropdownProps) {
         className={props.className}
         disabled={props.disabled}
         hasError={errorFlag}
-        height={props.height || "38px"}
+        height={props.height || getMinHeight(props.isMultiSelect)}
+        isMultiSelect={props.isMultiSelect}
         isOpen={isOpen}
         onClick={() => setIsOpen(!isOpen)}
         selected={!!selected}
@@ -742,14 +877,15 @@ export default function Dropdown(props: DropdownProps) {
         <SelectedValueNode
           hasError={errorFlag}
           hideSubText={props.hideSubText}
+          isMultiSelect={props.isMultiSelect}
           optionWidth={dropdownOptionWidth}
           placeholder={placeholder}
           renderNode={renderOption}
           selected={selected}
+          selectedOptionClickHandler={selectedOptionClickHandler}
           showDropIcon={showDropIcon}
           showLabelOnly={props.showLabelOnly}
         />
-        {}
         {isLoading ? (
           <Spinner size={IconSize.LARGE} />
         ) : (
@@ -776,7 +912,7 @@ export default function Dropdown(props: DropdownProps) {
     <DropdownContainer
       className={props.containerClassName + " " + replayHighlightClass}
       data-cy={props.cypressSelector}
-      height={props.height || "36px"}
+      height={getMinHeight(props.isMultiSelect)}
       tabIndex={0}
       width={dropdownWidth}
     >
@@ -793,6 +929,7 @@ export default function Dropdown(props: DropdownProps) {
         {dropdownTrigger}
         <RenderDropdownOptions
           {...props}
+          isMultiSelect={props.isMultiSelect}
           optionClickHandler={optionClickHandler}
           optionWidth={dropdownOptionWidth}
           selected={
@@ -804,4 +941,9 @@ export default function Dropdown(props: DropdownProps) {
       </Popover>
     </DropdownContainer>
   );
+}
+
+function getMinHeight(isMultiSelect?: boolean): string {
+  if (isMultiSelect) return "44px";
+  return "38px";
 }

cypress/integration/Smoke_TestSuite/ClientSideTests/ApiPaneTests/API_Edit_spec.js
@@ -38,7 +38,7 @@ describe("API Panel Test Functionality", function() {
     cy.log("Creation of FirstAPI Action successful");
     cy.enterDatasourceAndPath(testdata.baseUrl, testdata.methods);
     cy.get(apiwidget.settings).click({ force: true });
-    cy.get(apiwidget.confirmBeforeExecute).click();
+    cy.get(apiwidget.confirmBeforeExecute).click({ force: true });
     cy.get(apiwidget.runQueryButton).click();
     cy.get(".bp3-dialog")
       .find("button")

cypress/integration/Smoke_TestSuite/ClientSideTests/Binding/BindApi_withPageload_Input_spec.js
@@ -1,6 +1,5 @@
 const testdata = require("../../../../fixtures/testdata.json");
 const apiwidget = require("../../../../locators/apiWidgetslocator.json");
-const commonlocators = require("../../../../locators/commonlocators.json");
 const dsl = require("../../../../fixtures/MultipleInput.json");
 const widgetsPage = require("../../../../locators/Widgets.json");
 const publish = require("../../../../locators/publishWidgetspage.json");

cypress/integration/Smoke_TestSuite/ClientSideTests/Replay/Replay_Editor_spec.js
@@ -4,7 +4,6 @@ const datasource = require("../../../../locators/DatasourcesEditor.json");
 const datasourceEditor = require("../../../../locators/DatasourcesEditor.json");
 const datasourceFormData = require("../../../../fixtures/datasources.json");
 const queryLocators = require("../../../../locators/QueryEditor.json");
-const jsEditorLocators = require("../../../../locators/JSEditor.json");
 
 describe("Undo/Redo functionality", function() {
   const modifierKey = Cypress.platform === "darwin" ? "meta" : "ctrl";

cypress/integration/Smoke_TestSuite/ServerSideTests/ActionExecution/Action_PageOnLoad_spec.js
@@ -29,12 +29,13 @@ describe("API Panel Test Functionality", function() {
   });
 
   it("Shows which action failed on action fail.", function() {
+    cy.wait(2000);
     cy.NavigateToAPI_Panel();
     cy.CreateAPI("PageLoadApi2");
     cy.enterDatasourceAndPath("https://abc.com", "users");
     cy.WaitAutoSave();
     cy.get("li:contains('Settings')").click({ force: true });
-    cy.get("[data-cy=executeOnLoad]").click({ force: true });
+    cy.get("[name=executeOnLoad]").click({ force: true });
 
     cy.wait("@setExecuteOnLoad");
 
@@ -44,6 +45,7 @@ describe("API Panel Test Functionality", function() {
     cy.wait("@updateLayout");
 
     cy.reload();
+    cy.wait(3000);
     cy.get(commonlocators.toastMsg).contains(
       `The action "PageLoadApi2" has failed.`,
     );

cypress/integration/Smoke_TestSuite/ServerSideTests/QueryPane/Mongo_Spec.js
@@ -35,7 +35,6 @@ describe("Create a query with a mongo datasource, run, save and then delete the 
     // );
 
     cy.validateNSelectDropdown("Commands", "Find Document(s)", "Raw");
-
     cy.get(queryLocators.templateMenu).click();
     cy.typeValueNValidate('{"find": "listingsAndReviews","limit": 10}');
 

commit 3811d875a781ad5c2004a0514ef256498ff9cc1a

feat: Add keyboard navigation for dropdown (#9561)
* Add keydown handler for DropdownContainer

* Add keyboard focus state

* fix: esc resets changed selection

* Add tests

* fix: Merge induced type issue with multi-select option

* fix: broken locator from #8777

src/components/ads/Dropdown.tsx
@@ -21,6 +21,7 @@ import Tooltip from "components/ads/Tooltip";
 import { isEllipsisActive } from "utils/helpers";
 import SegmentHeader from "components/ads/ListSegmentHeader";
 import { useTheme } from "styled-components";
+import { findIndex } from "lodash";
 
 export type DropdownOnSelect = (value?: string, dropdownOption?: any) => void;
 
@@ -117,15 +118,6 @@ export interface RenderDropdownOptionType {
   option: DropdownOption | DropdownOption[];
   optionClickHandler?: (dropdownOption: DropdownOption) => void;
 }
-
-export const DropdownContainer = styled.div<{ width: string; height?: string }>`
-  width: ${(props) => props.width};
-  min-height: ${(props) => props.height};
-  position: relative;
-  span.bp3-popover-target {
-    display: inline-block;
-  }
-`;
 
 const DropdownTriggerWrapper = styled.div<{
   isOpen: boolean;
@@ -256,6 +248,19 @@ const Selected = styled.div<{
           ? Colors.FAIR_PINK
           : props.theme.colors.dropdown.hovered.bg
         : Colors.WHITE}
+`;
+
+export const DropdownContainer = styled.div<{ width: string; height?: string }>`
+  width: ${(props) => props.width};
+  height: ${(props) => props.height || `38px`};
+  position: relative;
+  span.bp3-popover-target {
+    display: inline-block;
+  }
+
+  &:focus-visible ${Selected} {
+    border: 1px solid var(--appsmith-input-focus-border-color);
+  }
 `;
 
 const DropdownSelect = styled.div``;
@@ -692,9 +697,11 @@ export function RenderDropdownOptions(props: DropdownOptionsProps) {
           }
           return !option.isSectionHeader ? (
             <OptionWrapper
+              aria-selected={isSelected}
               className="t--dropdown-option"
               key={index}
               onClick={() => props.optionClickHandler(option)}
+              role="option"
               selected={isSelected}
             >
               {option.leftElement && (
@@ -834,6 +841,67 @@ export default function Dropdown(props: DropdownProps) {
     }
   };
 
+  const handleKeydown = useCallback(
+    (e: React.KeyboardEvent) => {
+      switch (e.key) {
+        case "Escape":
+          if (isOpen) {
+            setSelected((prevSelected) => {
+              if (prevSelected != props.selected) return props.selected;
+              return prevSelected;
+            });
+            setIsOpen(false);
+            e.nativeEvent.stopImmediatePropagation();
+          }
+          break;
+        case " ":
+        case "Enter":
+          e.preventDefault();
+          if (isOpen && !("length" in selected)) optionClickHandler(selected);
+          else onClickHandler();
+          break;
+        case "ArrowUp":
+          e.preventDefault();
+          if (isOpen) {
+            setSelected((prevSelected) => {
+              if (!("length" in prevSelected)) {
+                let index = findIndex(props.options, prevSelected);
+                if (index === 0) index = props.options.length - 1;
+                else index--;
+                return props.options[index];
+              }
+              return prevSelected;
+            });
+          } else {
+            onClickHandler();
+          }
+          break;
+        case "ArrowDown":
+          e.preventDefault();
+          if (isOpen) {
+            setSelected((prevSelected) => {
+              if (!("length" in prevSelected)) {
+                let index = findIndex(props.options, prevSelected);
+                if (index === props.options.length - 1) index = 0;
+                else index++;
+                return props.options[index];
+              }
+              return prevSelected;
+            });
+          } else {
+            onClickHandler();
+          }
+          break;
+        case "Tab":
+          if (isOpen) {
+            setIsOpen(false);
+          }
+          break;
+      }
+    },
+    [isOpen, props.options, props.selected, selected],
+  );
+
   const [dropdownWrapperWidth, setDropdownWrapperWidth] = useState<string>(
     "100%",
   );
@@ -913,6 +981,8 @@ export default function Dropdown(props: DropdownProps) {
       className={props.containerClassName + " " + replayHighlightClass}
       data-cy={props.cypressSelector}
       height={getMinHeight(props.isMultiSelect)}
+      onKeyDown={handleKeydown}
+      role="listbox"
       tabIndex={0}
       width={dropdownWidth}
     >
@@ -932,11 +1002,7 @@ export default function Dropdown(props: DropdownProps) {
           isMultiSelect={props.isMultiSelect}
           optionClickHandler={optionClickHandler}
           optionWidth={dropdownOptionWidth}
-          selected={
-            props.selected
-              ? props.selected
-              : { id: undefined, value: undefined }
-          }
+          selected={selected ? selected : { id: undefined, value: undefined }}
         />
       </Popover>
     </DropdownContainer>

cypress/integration/Smoke_TestSuite/ServerSideTests/QueryPane/ConfirmRunAction_spec.js
@@ -15,11 +15,8 @@ describe("Confirm run action", function() {
   });
 
   it("Confirm run action", () => {
-    cy.NavigateToQueryEditor();
+    cy.NavigateToActiveDSQueryPane(datasourceName);
 
-    cy.contains(".t--datasource-name", datasourceName)
-      .find(queryLocators.createQuery)
-      .click();
     cy.get(queryLocators.templateMenu).click();
     cy.get(".CodeMirror textarea")
       .first()
@@ -27,7 +24,7 @@ describe("Confirm run action", function() {
       .type("select * from configs");
     cy.get("li:contains('Settings')").click({ force: true });
     cy.get("[data-cy=confirmBeforeExecute]")
-      .find(".bp3-switch")
+      .find("span")
       .click();
 
     cy.onlyQueryRun();

src/components/ads/Dropdown.test.tsx
@@ -1,0 +1,157 @@
+import React from "react";
+import "@testing-library/jest-dom";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import { ThemeProvider } from "constants/DefaultTheme";
+import Dropdown from "./Dropdown";
+import { lightTheme } from "selectors/themeSelectors";
+import userEvent from "@testing-library/user-event";
+
+const props = {
+  options: [
+    { label: "Primary", value: "PRIMARY" },
+    { label: "Secondary", value: "SECONDARY" },
+    { label: "Tertiary", value: "TERTIARY" },
+  ],
+  selected: {
+    label: "Primary",
+    value: "PRIMARY",
+  },
+  showLabelOnly: true,
+};
+
+describe("<Dropdown />", () => {
+  const getTestComponent = (handleOnSelect: any = undefined) => (
+    <ThemeProvider theme={lightTheme}>
+      <Dropdown
+        containerClassName="dropdown-container"
+        onSelect={handleOnSelect}
+        options={props.options}
+        selected={props.selected}
+        showLabelOnly={props.showLabelOnly}
+      />
+      ,
+    </ThemeProvider>
+  );
+
+  it("Pressing tab should focus the component", () => {
+    render(getTestComponent());
+    userEvent.tab();
+    expect(screen.getByRole("listbox")).toHaveFocus();
+  });
+
+  // Tests various ways to open dropdown
+  it.each(["{Enter}", " ", "{ArrowDown}", "{ArrowUp}"])(
+    "Once focused, pressing '%s' should open dropdown",
+    async (key) => {
+      render(getTestComponent());
+      expect(screen.queryByRole("option")).toBeNull();
+      userEvent.tab();
+
+      userEvent.keyboard(key);
+      expect(screen.queryAllByRole("option")).toHaveLength(3);
+
+      // Escape to close opened dropdown
+      userEvent.keyboard("{Escape}");
+      await waitForElementToBeRemoved(screen.getAllByRole("option"));
+    },
+  );
+
+  it("Pressing tab once dropdown is opened should close dropdown", async () => {
+    render(getTestComponent());
+    userEvent.tab();
+    userEvent.keyboard("{Enter}");
+    expect(screen.getAllByRole("option")).toHaveLength(3);
+    userEvent.tab();
+    await waitForElementToBeRemoved(screen.getAllByRole("option"));
+  });
+
+  it("{ArrowDown} should select next item", () => {
+    render(getTestComponent());
+    userEvent.tab();
+    expect(screen.getByRole("listbox")).toHaveTextContent("Primary");
+    userEvent.keyboard("{Enter}");
+
+    // Make sure first item is selected by default
+    expect(screen.getByRole("option", { selected: true })).toHaveTextContent(
+      "Primary",
+    );
+    expect(screen.getByRole("listbox")).toHaveTextContent("Primary");
+
+    // Press ArrowDown and check results
+    [
+      "Secondary",
+      "Tertiary",
+      // ArrowDown on last item should select first item
+      "Primary",
+    ].forEach((text) => {
+      userEvent.keyboard("{ArrowDown}");
+      expect(screen.getByRole("option", { selected: true })).toHaveTextContent(
+        text,
+      );
+      expect(screen.getByRole("listbox")).toHaveTextContent(text);
+    });
+  });
+
+  it("{ArrowUp} should select previous item", () => {
+    render(getTestComponent());
+    userEvent.tab();
+    expect(screen.getByRole("listbox")).toHaveTextContent("Primary");
+    userEvent.keyboard("{Enter}");
+
+    // Make sure first item is selected by default
+    expect(screen.getByRole("option", { selected: true })).toHaveTextContent(
+      "Primary",
+    );
+
+    // Press ArrowUp and check results
+    [
+      // ArrowUp on first item should select last item
+      "Tertiary",
+      "Secondary",
+      "Primary",
+    ].forEach((text) => {
+      userEvent.keyboard("{ArrowUp}");
+      expect(screen.getByRole("option", { selected: true })).toHaveTextContent(
+        text,
+      );
+      expect(screen.getByRole("listbox")).toHaveTextContent(text);
+    });
+  });
+
+  it("After selecting an option using arrow, {enter} or ' ' should trigger optionClick", () => {
+    const handleOnSelect = jest.fn();
+    render(getTestComponent(handleOnSelect));
+    userEvent.tab();
+    userEvent.keyboard("{Enter}");
+    userEvent.keyboard("{ArrowDown}");
+    userEvent.keyboard("{Enter}");
+    expect(handleOnSelect).toHaveBeenLastCalledWith(
+      props.options[1].value,
+      props.options[1],
+    );
+    userEvent.keyboard("{Enter}");
+    userEvent.keyboard("{ArrowDown}");
+    userEvent.keyboard(" ");
+    expect(handleOnSelect).toHaveBeenLastCalledWith(
+      props.options[2].value,
+      props.options[2],
+    );
+  });
+
+  it("After selecting an option using arrow, {Escape} should not trigger optionClick", () => {
+    const handleOnSelect = jest.fn();
+    render(getTestComponent());
+    userEvent.tab();
+    expect(screen.getByRole("listbox")).toHaveTextContent("Primary");
+    userEvent.keyboard("{Enter}");
+    userEvent.keyboard("{ArrowDown}");
+    expect(screen.getByRole("listbox")).toHaveTextContent("Secondary");
+    userEvent.keyboard("{Escape}");
+    expect(screen.getByRole("listbox")).toHaveTextContent("Primary");
+    expect(handleOnSelect).not.toHaveBeenCalled();
+  });
+});