Zeral-Zhang/appsmith

commit 434e4ef1c1a74691cfd2c25ddf4ddf281cd529d1

feat: URL Navigation for Widgets (#20202)
## Description

Widget selection is driven by URL changes. This would fix browser
navigation for users as they can use browser back/forward buttons to
travel across older contexts on Appsmith.

> Fixing browser URL navigation for widgets

Fixes #19571


Media
> A video or a GIF is preferred. when using Loom, don’t embed because it
looks like it’s a GIF. instead, just link to the video


## Type of change

> Please delete options that are not relevant.

- New feature (non-breaking change which adds functionality)


## How Has This Been Tested?

- Manual
- Jest
- Cypress

### Test Plan
> Add Testsmith [test
cases](https://github.com/appsmithorg/TestSmith/issues/2171) links that
relate to this PR

### Issues raised during DP testing
- [X] When a selected widget is below viewport and user refreshes the
page, then the widget property pane is open but the page does not
navigate to the selected widget
https://loom.com/share/09f1eda2f02d474981a0d48e4a6419ec

- [ ] Drop 2 widgets one at a time > Delete both the widgets > Now click
on back button of the browser > Observe the url it shows the widget id
in the URL but the canvas remains empty
https://loom.com/share/53cae28a5d224e67b783c8ccf53745f5
Dev Response: This issue is valid but not a major inconvenience. We will
try to track it and see if it needed to be addressed. Many other web
tools do not handle such cases


- [X] Canvas scrolls down when all widgets are selected.
https://loom.com/share/c8a68dadcdb040779abd3a73bde2b06c

- [X] Widget is not getting highlighted when added from the API editor
page. Please refer to the attached
video:-https://jiju8jbmwa.vmaker.com/record/IkwiAqFgafK9dVmu



## Checklist:
### Dev activity
- [ ] My code follows the style guidelines of this project
- [ ] I have performed a self-review of my own code
- [ ] I have commented my code, particularly in hard-to-understand areas

src/pages/common/CanvasArenas/hooks/useCanvasDragging.ts
@@ -574,7 +574,7 @@ export const useCanvasDragging = (
         resetCanvasState();
       }
     }
-  }, [isDragging, isResizing, blocksToDraw, snapRows, canExtend]);
+  }, [isDragging, isResizing, blocksToDraw, snapRows, canExtend, scale]);
   return {
     showCanvas: isDragging && !isResizing,
   };

src/sagas/WidgetSelectionSagas.ts
@@ -3,14 +3,21 @@ import {
   ReduxActionErrorTypes,
   ReduxActionTypes,
 } from "@appsmith/constants/ReduxActionConstants";
-import { all, call, fork, put, select, takeLatest } from "redux-saga/effects";
+import {
+  all,
+  call,
+  fork,
+  put,
+  select,
+  take,
+  takeLatest,
+} from "redux-saga/effects";
 import {
   getWidgetIdsByType,
   getWidgetImmediateChildren,
   getWidgets,
 } from "./selectors";
 import {
-  setLastSelectedWidget,
   setSelectedWidgets,
   WidgetSelectionRequestPayload,
 } from "actions/widgetSelectionActions";
@@ -18,10 +25,18 @@ import { getLastSelectedWidget, getSelectedWidgets } from "selectors/ui";
 import { CanvasWidgetsReduxState } from "reducers/entityReducers/canvasWidgetsReducer";
 import { AppState } from "@appsmith/reducers";
 import { closeAllModals, showModal } from "actions/widgetActions";
-import history from "utils/history";
-import { getCurrentPageId } from "selectors/editorSelectors";
-import { builderURL } from "RouteBuilder";
-import { getParentModalId } from "selectors/entitiesSelector";
+import history, { NavigationMethod } from "utils/history";
+import {
+  getCurrentPageId,
+  getIsEditorInitialized,
+  snipingModeSelector,
+} from "selectors/editorSelectors";
+import { builderURL, widgetURL } from "RouteBuilder";
+import {
+  getAppMode,
+  getCanvasWidgets,
+  getParentModalId,
+} from "selectors/entitiesSelector";
 import {
   assertParentId,
   isInvalidSelectionRequest,
@@ -36,12 +51,18 @@ import {
   unselectWidget,
 } from "sagas/WidgetSelectUtils";
 import { inGuidedTour } from "selectors/onboardingSelectors";
-import { MAIN_CONTAINER_WIDGET_ID } from "constants/WidgetConstants";
+import { flashElementsById, quickScrollToWidget } from "utils/helpers";
 import { areArraysEqual } from "utils/AppsmithUtils";
+import { APP_MODE } from "entities/App";
 
 function* selectWidgetSaga(action: ReduxAction<WidgetSelectionRequestPayload>) {
   try {
-    const { payload = [], selectionRequestType } = action.payload;
+    const {
+      payload = [],
+      selectionRequestType,
+      invokedBy,
+      pageId,
+    } = action.payload;
 
     if (payload.some(isInvalidSelectionRequest)) {
       // Throw error
@@ -70,10 +91,11 @@ function* selectWidgetSaga(action: ReduxAction<WidgetSelectionRequestPayload>) {
 
     switch (selectionRequestType) {
       case SelectionRequestType.Empty: {
-        newSelection = {
-          widgets: [],
-          lastWidgetSelected: MAIN_CONTAINER_WIDGET_ID,
-        };
+        newSelection = [];
+        break;
+      }
+      case SelectionRequestType.UnsafeSelect: {
+        newSelection = payload;
         break;
       }
       case SelectionRequestType.One: {
@@ -129,32 +151,29 @@ function* selectWidgetSaga(action: ReduxAction<WidgetSelectionRequestPayload>) {
       [SelectionRequestType.PushPop, SelectionRequestType.ShiftSelect].includes(
         selectionRequestType,
       ) &&
-      newSelection.widgets[0] in allWidgets
+      newSelection[0] in allWidgets
     ) {
-      const selectionWidgetId = newSelection.widgets[0];
+      const selectionWidgetId = newSelection[0];
       const parentId = allWidgets[selectionWidgetId].parentId;
       if (parentId) {
         const selectionSiblingWidgets: string[] = yield select(
           getWidgetImmediateChildren,
           parentId,
         );
-        newSelection.widgets = newSelection.widgets.filter((each) =>
+        newSelection = newSelection.filter((each) =>
           selectionSiblingWidgets.includes(each),
         );
       }
     }
-    if (!areArraysEqual(newSelection.widgets, selectedWidgets)) {
-      yield put(setSelectedWidgets(newSelection.widgets));
-    }
-    if (parentId && newSelection.widgets.length === 1) {
+
+    if (parentId && newSelection.length === 1) {
       yield call(setWidgetAncestry, parentId, allWidgets);
     }
-    if (
-      newSelection.lastWidgetSelected &&
-      newSelection.lastWidgetSelected !== lastSelectedWidget
-    ) {
-      yield put(setLastSelectedWidget(newSelection.lastWidgetSelected));
+    if (areArraysEqual([...newSelection], [...selectedWidgets])) {
+      yield call(focusOnWidgetSaga, setSelectedWidgets(newSelection));
+      return;
     }
+    yield call(appendSelectedWidgetToUrlSaga, newSelection, pageId, invokedBy);
   } catch (error) {
     yield put({
       type: ReduxActionErrorTypes.WIDGET_SELECTION_ERROR,
@@ -168,34 +187,35 @@ function* selectWidgetSaga(action: ReduxAction<WidgetSelectionRequestPayload>) {
 
 /**
  * Append Selected widgetId as hash to the url path
- * @param action
+ * @param selectedWidgets
+ * @param pageId
+ * @param invokedBy
  */
 function* appendSelectedWidgetToUrlSaga(
-  action: ReduxAction<{ selectedWidgets: string[] }>,
+  selectedWidgets: string[],
+  pageId?: string,
+  invokedBy?: NavigationMethod,
 ) {
   const guidedTourEnabled: boolean = yield select(inGuidedTour);
-  if (guidedTourEnabled) return;
-  const { hash, pathname } = window.location;
-  const { selectedWidgets } = action.payload;
+  const isSnipingMode: boolean = yield select(snipingModeSelector);
+  const appMode: APP_MODE = yield select(getAppMode);
+  const viewMode = appMode === APP_MODE.PUBLISHED;
+  if (guidedTourEnabled || isSnipingMode || viewMode) return;
+  const { pathname } = window.location;
   const currentPageId: string = yield select(getCurrentPageId);
-
-  const currentURL = hash ? `${pathname}${hash}` : pathname;
-  let canvasEditorURL;
-  if (selectedWidgets.length === 1) {
-    canvasEditorURL = `${builderURL({
-      pageId: currentPageId,
-      hash: selectedWidgets[0],
-      persistExistingParams: true,
-    })}`;
-  } else {
-    canvasEditorURL = `${builderURL({
-      pageId: currentPageId,
-      persistExistingParams: true,
-    })}`;
-  }
-
-  if (currentURL !== canvasEditorURL) {
-    history.replace(canvasEditorURL);
+  const currentURL = pathname;
+  const newUrl = selectedWidgets.length
+    ? widgetURL({
+        pageId: pageId ?? currentPageId,
+        persistExistingParams: true,
+        selectedWidgets,
+      })
+    : builderURL({
+        pageId: pageId ?? currentPageId,
+        persistExistingParams: true,
+      });
+  if (currentURL !== newUrl) {
+    history.push(newUrl, { invokedBy });
   }
 }
 
@@ -210,6 +230,11 @@ function* canPerformSelectionSaga(saga: any, action: any) {
 
 function* openOrCloseModalSaga(action: ReduxAction<{ widgetIds: string[] }>) {
   if (action.payload.widgetIds.length !== 1) return;
+
+  const isEditorInitialized: boolean = yield select(getIsEditorInitialized);
+  if (!isEditorInitialized) {
+    yield take(ReduxActionTypes.INITIALIZE_EDITOR_SUCCESS);
+  }
 
   const selectedWidget = action.payload.widgetIds[0];
 
@@ -240,6 +265,25 @@ function* openOrCloseModalSaga(action: ReduxAction<{ widgetIds: string[] }>) {
   yield put(closeAllModals());
 }
 
+function* focusOnWidgetSaga(action: ReduxAction<{ widgetIds: string[] }>) {
+  if (action.payload.widgetIds.length > 1) return;
+  const widgetId = action.payload.widgetIds[0];
+  const isEditorInitialized: boolean = yield select(getIsEditorInitialized);
+  if (!isEditorInitialized) {
+    yield take(ReduxActionTypes.INITIALIZE_EDITOR_SUCCESS);
+  }
+  const allWidgets: CanvasWidgetsReduxState = yield select(getCanvasWidgets);
+  if (widgetId) {
+    setTimeout(() => {
+      // Scrolling will hide some part of the content at the top during guided tour. To avoid that
+      // we skip scrolling altogether during guided tour as we don't have
+      // too many widgets during the same
+      flashElementsById(widgetId);
+      quickScrollToWidget(widgetId, allWidgets);
+    }, 0);
+  }
+}
+
 export function* widgetSelectionSagas() {
   yield all([
     takeLatest(ReduxActionTypes.SELECT_WIDGET_INIT, selectWidgetSaga),
@@ -248,10 +292,6 @@ export function* widgetSelectionSagas() {
       canPerformSelectionSaga,
       openOrCloseModalSaga,
     ),
-    takeLatest(
-      ReduxActionTypes.APPEND_SELECTED_WIDGET_TO_URL,
-      canPerformSelectionSaga,
-      appendSelectedWidgetToUrlSaga,
-    ),
+    takeLatest(ReduxActionTypes.SET_SELECTED_WIDGETS, focusOnWidgetSaga),
   ]);
 }

src/components/editorComponents/GlobalSearch/index.tsx
@@ -102,6 +102,7 @@ const StyledContainer = styled.div<{ category: SearchCategory; query: string }>`
   padding: ${(props) => props.theme.spaces[5]}px;
   flex-direction: column;
   position: relative;
+
   & .main {
     display: flex;
     flex: 1;
@@ -110,6 +111,7 @@ const StyledContainer = styled.div<{ category: SearchCategory; query: string }>`
     background-color: ${(props) =>
       props.theme.colors.globalSearch.primaryBgColor};
   }
+
   ${algoliaHighlightTag},
   & .ais-Highlight-highlighted,
   & .search-highlighted {
@@ -407,6 +409,9 @@ function GlobalSearch() {
       activeItem.pageId,
       NavigationMethod.Omnibar,
       lastSelectedWidgetId === activeItem.widgetId,
+      false,
+      false,
+      activeItem.pageId !== currentPageId,
     );
   };
 

src/pages/Editor/Explorer/Pages/index.tsx
@@ -49,7 +49,6 @@ import {
   hasManagePagePermission,
 } from "@appsmith/utils/permissionHelpers";
 import { AppState } from "@appsmith/reducers";
-import { pageChanged } from "actions/focusHistoryActions";
 
 const ENTITY_HEIGHT = 36;
 const MIN_PAGES_HEIGHT = 60;
@@ -67,10 +66,12 @@ const StyledEntity = styled(Entity)<{ pagesSize?: number }>`
       overflow-y: auto;
     }
   }
+
   &.page .${EntityClassNames.PRE_RIGHT_ICON} {
     width: 20px;
     right: 0;
   }
+
   &.page:hover {
     & .${EntityClassNames.PRE_RIGHT_ICON} {
       display: none;
@@ -129,16 +130,6 @@ function Pages() {
       history.push(navigateToUrl, {
         invokedBy: NavigationMethod.EntityExplorer,
       });
-      const currentURL = navigateToUrl.split(/(?=\?)/g);
-      dispatch(
-        pageChanged(
-          page.pageId,
-          currentURL[0],
-          currentURL[1],
-          location.pathname,
-          location.search,
-        ),
-      );
     },
     [location.pathname],
   );

cypress/integration/Regression_TestSuite/Application/PgAdmin_spec.js
@@ -1,4 +1,5 @@
 import * as _ from "../../../support/Objects/ObjectsCore";
+
 const dsl = require("../../../fixtures/PgAdmindsl.json");
 const queryLocators = require("../../../locators/QueryEditor.json");
 const widgetsPage = require("../../../locators/Widgets.json");
@@ -40,7 +41,9 @@ describe("PgAdmin Clone App", function() {
     cy.get(queryLocators.templateMenu).click();
     // writing query to get all the tables
     _.dataSources.EnterQuery(
-      `select * from pg_catalog.pg_tables where schemaname = {{schema_select.selectedOptionValue || "public"}} ;`,
+      `select *
+       from pg_catalog.pg_tables
+       where schemaname = {{schema_select.selectedOptionValue || "public"}};`,
       2000,
     );
     _.dataSources.RunQuery();
@@ -53,7 +56,8 @@ describe("PgAdmin Clone App", function() {
     cy.get(queryLocators.templateMenu).click();
     // creating query to get the columns of the table
     _.dataSources.EnterQuery(
-      `SELECT column_name, data_type, table_name, ordinal_position, is_nullable FROM information_schema.COLUMNS`,
+      `SELECT column_name, data_type, table_name, ordinal_position, is_nullable
+       FROM information_schema.COLUMNS`,
     );
     _.dataSources.RunQuery();
 
@@ -65,7 +69,31 @@ describe("PgAdmin Clone App", function() {
     cy.get(queryLocators.templateMenu).click();
     // writing query to create new table
     _.dataSources.EnterQuery(
-      `CREATE TABLE {{schema_select.selectedOptionValue}}.{{nt_name.text.replaceAll(" ","_")}}({{appsmith.store.nt_col.map((c)=>c.name.replaceAll(" ","_") + " " + c.dtype + (c.nnull ? " NOT NULL " :  "") + (c.pkey ? " PRIMARY KEY " : "")).join(" , ")}})`,
+      `CREATE TABLE {{schema_select.selectedOptionValue}}.{{nt_name.text.replaceAll
+       (
+         " ",
+         "_"
+       )}}
+       (
+         {
+         {
+         appsmith.store.nt_col.map(
+       (
+         c
+       )=> c.name.replaceAll
+       (
+         " ",
+         "_"
+       ) + " " + c.dtype +
+       (
+         c.nnull ? " NOT NULL " : ""
+       ) +
+       (
+         c.pkey ? " PRIMARY KEY " : ""
+       )).join
+       (
+         " , "
+       )}})`,
     );
     // clicking on chevron icon to go back to the _.dataSources page
     cy.get(appPage.dropdownChevronLeft).click();

cypress/integration/Regression_TestSuite/ClientSideTests/ExplorerTests/Entity_Explorer_API_Pane_spec.js
@@ -3,6 +3,7 @@ const apiwidget = require("../../../../locators/apiWidgetslocator.json");
 const pageid = "MyPage";
 
 import { ObjectsRegistry } from "../../../../support/Objects/Registry";
+
 let ee = ObjectsRegistry.EntityExplorer,
   agHelper = ObjectsRegistry.AggregateHelper,
   locator = ObjectsRegistry.CommonLocators,
@@ -70,6 +71,7 @@ describe("Entity explorer API pane related testcases", function() {
       .click({ force: true });
     ee.ActionContextMenuByEntityName("SecondAPI", "Move to page", pageid);
     cy.wait(500);
+    ee.ExpandCollapseEntity("Queries/JS");
     ee.AssertEntityPresenceInExplorer("SecondAPI");
     /*To be enabled once the bug is fixed
     cy.get(apiwidget.propertyList).then(function($lis) {

cypress/integration/Regression_TestSuite/ClientSideTests/ExplorerTests/JSEditorContextMenu_Spec.ts
@@ -1,6 +1,6 @@
 import { ObjectsRegistry } from "../../../../support/Objects/Registry";
 
-let ee = ObjectsRegistry.EntityExplorer,
+const ee = ObjectsRegistry.EntityExplorer,
   jsEditor = ObjectsRegistry.JSEditor;
 
 describe("Validate basic operations on Entity explorer JSEditor structure", () => {
@@ -41,20 +41,19 @@ describe("Validate basic operations on Entity explorer JSEditor structure", () =
     ee.AddNewPage();
     ee.AssertEntityPresenceInExplorer(newPageId);
     ee.SelectEntityByName(pageId);
-    ee.ExpandCollapseEntity("Queries/JS");
     ee.ActionContextMenuByEntityName(
       "RenamedJSObjectCopy",
       "Move to page",
       newPageId,
     );
     ee.SelectEntityByName(newPageId);
+    ee.ExpandCollapseEntity("Queries/JS");
     ee.AssertEntityPresenceInExplorer("RenamedJSObjectCopy");
     jsEditor.ValidateDefaultJSObjProperties("RenamedJSObjectCopy");
   });
 
   it("6. Validate Deletion of JSObject", function() {
     ee.SelectEntityByName(pageId);
-    ee.ExpandCollapseEntity("Queries/JS");
     ee.ActionContextMenuByEntityName(
       "ExplorerRenamed",
       "Delete",

cypress/integration/Regression_TestSuite/ClientSideTests/Git/GitImport/GitImport_spec.js
@@ -188,10 +188,8 @@ describe("Git import flow ", function() {
       "response.body.responseMeta.status",
       201,
     );
-    cy.CheckAndUnfoldEntityItem("Queries/JS");
     // verify jsObject is not duplicated
-    cy.get(`.t--entity-name:contains(${jsObject})`).should("have.length", 1);
-    _.ee.SelectEntityByName(jsObject);
+    _.ee.SelectEntityByName(jsObject, "Queries/JS"); //Also checking jsobject exists after cloning the page
     _.jsEditor.RunJSObj(); //Running sync function due to open bug
     _.ee.SelectEntityByName("Page1 Copy");
     cy.xpath("//input[@class='bp3-input' and @value='Success']").should(
@@ -211,9 +209,9 @@ describe("Git import flow ", function() {
     cy.get(gitSyncLocators.closeGitSyncModal).click();
     cy.wait(2000);
     cy.latestDeployPreview();
-    cy.get(".tbody")
-      .first()
-      .should("contain.text", "Test user 7");
+    _.table.ReadTableRowColumnData(0, 1).then(($text) => {
+      expect($text).to.eq("Test user 7New Config");
+    }); //Checking both tables
     // verify api response binded to input widget
     cy.xpath("//input[@value='this is a test']");
     // verify js object binded to input widget
@@ -222,9 +220,9 @@ describe("Git import flow ", function() {
     cy.get(".t--page-switch-tab")
       .contains("Page1")
       .click({ force: true });
-    cy.get(".tbody")
-      .first()
-      .should("contain.text", "Test user 7");
+    _.table.ReadTableRowColumnData(0, 1).then(($text) => {
+      expect($text).to.eq("Test user 7New Config");
+    }); //Checking both tables
     // verify api response binded to input widget
     cy.xpath("//input[@value='this is a test']");
     // verify js object binded to input widget

commit 4b73e08a9ac57074f456e2d880b2b7a948ee3ccd

fix: Optimise Modal open performance (#21103)
## Description

#20202 had caused a performance regression in widget selection
specifically around opening and closing modals. It was noticed that
focusing on widget caused the increased rendering time.
Focusing involved:
- scrolling a widget into view
- flashing the widget

These two functions are not really needed when a modal is selected since
the modal already comes top and centre. Removing these two functions for
widgets solved the rendering performance issue.

Numbers:

| Test Case | Before Widget Selection | With Widget Selection | Current
PR |

|--------------------------|-------------------------|-----------------------|------------|
| Misc Open Complex Modal | 2035.65 | 2267.84 | 2101.03 |
| Misc Open Simple Modal | 1191.92 | 1156.78 | 1082.88 |
| Open Modal | 558.62 | 813.64 | 576.45 |
| Misc Close Complex Modal | 294.21 | 431.76 | 278.43 |
| Misc Close Simple Modal | 953.42 | 963.77 | 938.77 |
| Close Modal | 384.86 | 673 | 674.61 |


> Reverse performance regression introduced in #20202

Fixes #20901

## Type of change

- Bug fix (non-breaking change which fixes an issue)


## How Has This Been Tested?

- Performance Tests

### Test Plan
> Add Testsmith test cases links that relate to this PR

### Issues raised during DP testing
> Link issues raised during DP testing for better visiblity and tracking
(copy link from comments dropped on this PR)


## Checklist:
### Dev activity
- [ ] My code follows the style guidelines of this project
- [ ] I have performed a self-review of my own code
- [ ] I have commented my code, particularly in hard-to-understand areas
- [ ] I have made corresponding changes to the documentation
- [ ] My changes generate no new warnings
- [ ] I have added tests that prove my fix is effective or that my
feature works
- [ ] New and existing unit tests pass locally with my changes
- [ ] PR is being merged under a feature flag

src/pages/Editor/WidgetsEditor/index.tsx
@@ -4,7 +4,6 @@ import { useDispatch, useSelector } from "react-redux";
 import {
   getCurrentPageId,
   getCurrentPageName,
-  getIsFetchingPage,
 } from "selectors/editorSelectors";
 import PageTabs from "./PageTabs";
 import PerformanceTracker, {
@@ -12,7 +11,6 @@ import PerformanceTracker, {
 } from "utils/PerformanceTracker";
 import AnalyticsUtil from "utils/AnalyticsUtil";
 import CanvasContainer from "./CanvasContainer";
-import { quickScrollToWidget } from "utils/helpers";
 import Debugger from "components/editorComponents/Debugger";
 import OnboardingTasks from "../FirstTimeUserOnboarding/Tasks";
 import CrudInfoModal from "../GeneratePage/components/CrudInfoModal";
@@ -31,20 +29,16 @@ import PropertyPaneContainer from "./PropertyPaneContainer";
 import CanvasTopSection from "./EmptyCanvasSection";
 import { useAutoHeightUIState } from "utils/hooks/autoHeightUIHooks";
 import { isMultiPaneActive } from "selectors/multiPaneSelectors";
-import { getCanvasWidgets } from "selectors/entitiesSelector";
 
-/* eslint-disable react/display-name */
 function WidgetsEditor() {
-  const { deselectAll, focusWidget, selectWidget } = useWidgetSelection();
+  const { deselectAll, focusWidget } = useWidgetSelection();
   const dispatch = useDispatch();
   const currentPageId = useSelector(getCurrentPageId);
   const currentPageName = useSelector(getCurrentPageName);
   const currentApp = useSelector(getCurrentApplication);
-  const isFetchingPage = useSelector(getIsFetchingPage);
   const showOnboardingTasks = useSelector(getIsOnboardingTasksView);
   const guidedTourEnabled = useSelector(inGuidedTour);
   const isMultiPane = useSelector(isMultiPaneActive);
-  const canvasWidgets = useSelector(getCanvasWidgets);
 
   useEffect(() => {
     PerformanceTracker.stopTracking(PerformanceTransactionName.CLOSE_SIDE_PANE);
@@ -61,18 +55,6 @@ function WidgetsEditor() {
       });
     }
   }, [currentPageName, currentPageId]);
-
-  // navigate to widget
-  useEffect(() => {
-    if (
-      !isFetchingPage &&
-      window.location.hash.length > 0 &&
-      !guidedTourEnabled
-    ) {
-      const widgetIdFromURLHash = window.location.hash.slice(1);
-      quickScrollToWidget(widgetIdFromURLHash, canvasWidgets);
-    }
-  }, [isFetchingPage, selectWidget, guidedTourEnabled]);
 
   const allowDragToSelect = useAllowEditorDragToSelect();
   const { isAutoHeightWithLimitsChanging } = useAutoHeightUIState();

src/pages/common/CanvasArenas/hooks/useCanvasDragging.ts
@@ -574,7 +574,7 @@ export const useCanvasDragging = (
         resetCanvasState();
       }
     }
-  }, [isDragging, isResizing, blocksToDraw, snapRows, canExtend, scale]);
+  }, [isDragging, isResizing, blocksToDraw, snapRows, canExtend]);
   return {
     showCanvas: isDragging && !isResizing,
   };

src/sagas/WidgetSelectionSagas.ts
@@ -50,7 +50,7 @@ import {
   shiftSelectWidgets,
   unselectWidget,
 } from "sagas/WidgetSelectUtils";
-import { flashElementsById, quickScrollToWidget } from "utils/helpers";
+import { quickScrollToWidget } from "utils/helpers";
 import { areArraysEqual } from "utils/AppsmithUtils";
 import { APP_MODE } from "entities/App";
 
@@ -272,13 +272,7 @@ function* focusOnWidgetSaga(action: ReduxAction<{ widgetIds: string[] }>) {
   }
   const allWidgets: CanvasWidgetsReduxState = yield select(getCanvasWidgets);
   if (widgetId) {
-    setTimeout(() => {
-      // Scrolling will hide some part of the content at the top during guided tour. To avoid that
-      // we skip scrolling altogether during guided tour as we don't have
-      // too many widgets during the same
-      flashElementsById(widgetId);
-      quickScrollToWidget(widgetId, allWidgets);
-    }, 0);
+    quickScrollToWidget(widgetId, allWidgets);
   }
 }
 

src/utils/helpers.tsx
@@ -230,8 +230,7 @@ export const quickScrollToWidget = (
   canvasWidgets: CanvasWidgetsReduxState,
 ) => {
   if (!widgetId || widgetId === "") return;
-
-  setTimeout(() => {
+  window.requestIdleCallback(() => {
     const el = document.getElementById(widgetId);
     const canvas = document.getElementById("canvas-viewport");
 
@@ -245,7 +244,7 @@ export const quickScrollToWidget = (
         });
       }
     }
-  }, 200);
+  });
 };
 
 // Checks if the element in a container is visible or not.
@@ -276,7 +275,9 @@ function getWidgetElementToScroll(
   const parentId = widget.parentId || "";
   const containerId = getContainerIdForCanvas(parentId);
   if (containerId === MAIN_CONTAINER_WIDGET_ID) {
-    return document.getElementById(widgetId);
+    if (widget.type !== "MODAL_WIDGET") {
+      return document.getElementById(widgetId);
+    }
   }
   const containerWidget = canvasWidgets[containerId] as ContainerWidgetProps<
     WidgetProps