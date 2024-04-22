Zeral-Zhang/appsmith

commit ef279d1e767b08fec71dd7517cdb44bf722aae61

feat: update copy from widgets to UI elements (#32514)
## Description
> [!TIP]  
With the addition of building blocks to the explorer items, we need to
change the widget name to something that better encapsulates the
contents of the canvas explorer. All instances of "widgets" in the
canvas explorer have been changed to "UI elements"

Fixes #32331

## Automation

/ok-to-test tags="@tag.Widget, @tag.MainContainer, @tag.Visual"

### :mag: Cypress test results
<!-- This is an auto-generated comment: Cypress test results  -->
> [!TIP]
> 🟢 🟢 🟢 All cypress tests have passed! 🎉 🎉 🎉
> Workflow run:
<https://github.com/appsmithorg/appsmith/actions/runs/8777493414>
> Commit: 97f5cdb47a40086bf2979661c39fa4a32d0e4dd8
> Cypress dashboard url: <a
href="https://internal.appsmith.com/app/cypress-dashboard/rundetails-65890b3c81d7400d08fa9ee5?branch=master&workflowId=8777493414&attempt=1"
target="_blank">Click here!</a>

<!-- end of auto-generated comment: Cypress test results  -->












<!-- This is an auto-generated comment: release notes by coderabbit.ai
-->

## Summary by CodeRabbit

- **New Features**
- Added new constants and updated text descriptions for UI elements in
the editor pane.
	- Enhanced the multi-select property pane with clearer text.
- **Refactor**
- Changed terminology from "widgets" to "UI elements" across various UI
components.
- **Style**
- Updated messages and placeholder texts to improve user interface
clarity and consistency.

<!-- end of auto-generated comment: release notes by coderabbit.ai -->

app/client/src/ce/constants/messages.ts
@@ -1535,6 +1535,7 @@ export const QUERY_CONFIRMATION_MODAL_MESSAGE = () =>
 export const ENTITY_EXPLORER_TITLE = () => "NAVIGATION";
 export const MULTI_SELECT_PROPERTY_PANE_MESSAGE = () =>
   `Select a widget to see it's properties`;
+export const WIDGET_MULTI_SELECT = () => "Multiple selected";
 export const WIDGET_DEPRECATION_MESSAGE = (widgetName: string) =>
   `Drag the latest ${widgetName} to unlock new features and prevent end-of-life headaches for this widget.`;
 
@@ -2313,13 +2314,13 @@ export const EDITOR_PANE_TEXTS = {
   js_blank_state_description: () =>
     "Use JS to transform your data or write business logic",
   widget_blank_state_description: () =>
-    "Drag & drop widgets to create your app",
+    "Drag & drop UI elements to create your app",
   query_add_button: () => "New query / API",
   js_add_button: () => "New JS object",
   js_blank_object_item: () => "Blank JS object",
-  widget_add_button: () => "New widget",
+  widget_add_button: () => "New UI element",
   query_create_tab_title: () => "Create new query / API",
-  widgets_create_tab_title: () => "Drag & drop a widget",
+  widgets_create_tab_title: () => "Drag & drop UI elements",
   js_create_tab_title: () => "Create JS object",
   queries_create_from_existing: () => "From existing datasource",
   queries_create_new: () => "New API",
@@ -2452,7 +2453,9 @@ export const CUSTOM_WIDGET_FEATURE = {
 };
 
 export const WIDGET_PANEL_EMPTY_MESSAGE = () =>
-  "We couldn’t find any widgets called";
+  "We couldn’t find any UI elements called";
+
+export const UI_ELEMENT_PANEL_SEARCH_TEXT = () => "Search UI elements";
 
 export const HTTP_PROTOCOL_INPUT_PLACEHOLDER = () => `Select HTTP Protocol`;
 

app/client/src/pages/Editor/MultiSelectPropertyPane/index.tsx
@@ -3,6 +3,7 @@ import * as Sentry from "@sentry/react";
 import {
   createMessage,
   MULTI_SELECT_PROPERTY_PANE_MESSAGE,
+  WIDGET_MULTI_SELECT,
 } from "@appsmith/constants/messages";
 import { Text, Button } from "design-system";
 import { useSelector } from "react-redux";
@@ -20,7 +21,7 @@ function MultiSelectPropertyPane() {
     <div className="relative space-y-3">
       <div className="px-3 py-3">
         <Text kind="heading-s" renderAs="h3">
-          Multi
+          {createMessage(WIDGET_MULTI_SELECT)}
         </Text>
       </div>
 

app/client/src/pages/Editor/widgetSidebar/UIEntitySidebar.tsx
@@ -1,4 +1,5 @@
 import {
+  UI_ELEMENT_PANEL_SEARCH_TEXT,
   WIDGET_PANEL_EMPTY_MESSAGE,
   createMessage,
 } from "@appsmith/constants/messages";
@@ -103,7 +104,7 @@ function UIEntitySidebar({
           autoComplete="off"
           id={ENTITY_EXPLORER_SEARCH_ID}
           onChange={search}
-          placeholder="Search widgets"
+          placeholder={createMessage(UI_ELEMENT_PANEL_SEARCH_TEXT)}
           ref={searchInputRef}
           type="text"
         />

commit 16fcb1a9e9020b8dcbb79be6935408d3cbea3539

fix: API_Pane_spec - Fixed new messages (#32848)
## Description
New text needed to be updated

caused by https://github.com/appsmithorg/appsmith/pull/32514


Fixes #`Issue Number`  
_or_  
Fixes `Issue URL`
> [!WARNING]  
> _If no issue exists, please create an issue first, and check with the
maintainers if the issue is valid._

## Automation

/ok-to-test tags=""

### :mag: Cypress test results
<!-- This is an auto-generated comment: Cypress test results  -->
> [!CAUTION]  
> If you modify the content in this section, you are likely to disrupt
the CI result for your PR.

<!-- end of auto-generated comment: Cypress test results  -->


## Communication
Should the DevRel and Marketing teams inform users about this change?
- [ ] Yes
- [ ] No


<!-- This is an auto-generated comment: release notes by coderabbit.ai
-->
## Summary by CodeRabbit


- **Tests**
- Enhanced API pane tests to use centralized text management for
improved consistency and maintainability.
- Updated test configurations to reflect the renamed test specification
file.
    - Improved import statements for better code organization.
- Modified text content within `AssertElementVisibility` calls for
clarity.
- Added import statements for `createMessage` and `EDITOR_PANE_TEXTS`.
    - Updated import path for `createMessage`.
- Modified text content within `AssertElementVisibility` calls to use
`createMessage` with `EDITOR_PANE_TEXTS`.

<!-- end of auto-generated comment: release notes by coderabbit.ai -->

cypress/e2e/Regression/ClientSide/ExplorerTests/API_Pane_spec.js
@@ -3,6 +3,10 @@ import EditorNavigation, {
   PageLeftPane,
   PagePaneSegment,
 } from "../../../../support/Pages/EditorNavigation";
+import {
+  createMessage,
+  EDITOR_PANE_TEXTS,
+} from "../../../../../src/ce/constants/messages";
 
 const testdata = require("../../../../fixtures/testdata.json");
 const apiwidget = require("../../../../locators/apiWidgetslocator.json");
@@ -24,33 +28,47 @@ describe(
       homePage.CreateAppInWorkspace("EmptyMsgCheck");
       PageLeftPane.switchSegment(PagePaneSegment.UI);
       agHelper.AssertElementVisibility(
-        locator._visibleTextSpan("Drag & drop widgets to create your app"),
-      );
-      agHelper.AssertElementVisibility(locator._visibleTextSpan("New widget"));
-      PageLeftPane.switchSegment(PagePaneSegment.Queries);
-      agHelper.AssertElementVisibility(
         locator._visibleTextSpan(
-          "Write your first query or API to access data",
+          createMessage(EDITOR_PANE_TEXTS.widget_blank_state_description),
         ),
       );
       agHelper.AssertElementVisibility(
-        locator._visibleTextSpan("No queries to display"),
+        locator._visibleTextSpan(
+          createMessage(EDITOR_PANE_TEXTS.widget_add_button),
+        ),
+      );
+      PageLeftPane.switchSegment(PagePaneSegment.Queries);
+      agHelper.AssertElementVisibility(
+        locator._visibleTextSpan(
+          createMessage(EDITOR_PANE_TEXTS.query_blank_state_description),
+        ),
       );
       agHelper.AssertElementVisibility(
-        locator._visibleTextSpan("New query / API"),
+        locator._visibleTextSpan(
+          createMessage(EDITOR_PANE_TEXTS.query_blank_state),
+        ),
+      );
+      agHelper.AssertElementVisibility(
+        locator._visibleTextSpan(
+          createMessage(EDITOR_PANE_TEXTS.query_add_button),
+        ),
       );
 
       PageLeftPane.switchSegment(PagePaneSegment.JS);
       agHelper.AssertElementVisibility(
         locator._visibleTextSpan(
-          "Use JS to transform your data or write business logic",
+          createMessage(EDITOR_PANE_TEXTS.js_blank_state_description),
         ),
       );
       agHelper.AssertElementVisibility(
-        locator._visibleTextSpan("No JS objects to display"),
+        locator._visibleTextSpan(
+          createMessage(EDITOR_PANE_TEXTS.js_blank_state),
+        ),
       );
       agHelper.AssertElementVisibility(
-        locator._visibleTextSpan("New JS object"),
+        locator._visibleTextSpan(
+          createMessage(EDITOR_PANE_TEXTS.js_add_button),
+        ),
       );
     });
 